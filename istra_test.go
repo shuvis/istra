ce989871282c31979f276ca3fdb188a4dea7bfd7
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2020-01-14 12:27:38 +0200

    test for default config

--- istra_test.go
+++ istra_test.go
@@ -7,7 +7,10 @@ import (
 	"testing"
 )
 
-const channel = "channel"
+const (
+	channel     = "channel"
+	channelName = "name"
+)
 
 func Test_ProcessQueue(t *testing.T) {
 
@@ -40,7 +43,7 @@ func Test_ProcessQueue(t *testing.T) {
 		closeChan := make(chan error)
 		channel := ConsumerChannel{}
 		amqp := &AmqpMock{Ch: &channel, CloseChan: closeChan}
-		conf := QueueConf{Name: "name", AutoAck: true, Exclusive: true, NoLocal: true, NoWait: true}
+		conf := QueueConf{Name: channelName, AutoAck: true, Exclusive: true, NoLocal: true, NoWait: true}
 
 		wg := sync.WaitGroup{}
 		wg.Add(1)
@@ -51,9 +54,25 @@ func Test_ProcessQueue(t *testing.T) {
 		closeChan <- errors.New("channel closed")
 		wg.Wait()
 
-		if !reflect.DeepEqual(conf, channel.Conf) {
-			t.Errorf("wanted calls %v got %v", conf, channel.Conf)
-		}
+		assertConfig(t, channel.Conf, channelName, true, true, true, true)
+	})
+
+	t.Run("test channel consumed default config", func(t *testing.T) {
+		closeChan := make(chan error)
+		channel := ConsumerChannel{}
+		amqp := &AmqpMock{Ch: &channel, CloseChan: closeChan}
+		conf := QueueConf{Name: channelName}
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ProcessQueue(amqp, conf, func(msg []byte) {})
+		}()
+		closeChan <- errors.New("channel closed")
+		wg.Wait()
+
+		assertConfig(t, channel.Conf, channelName, false, false, false, false)
 	})
 
 	t.Run("test channel return error", func(t *testing.T) {
@@ -91,6 +110,25 @@ func Test_ProcessQueue(t *testing.T) {
 			t.Errorf("wanted calls %v got %v", want, result)
 		}
 	})
+}
+
+func assertConfig(t *testing.T, conf QueueConf, name string, autoAck bool, exclusive bool, noLocal bool, noWait bool) {
+	t.Helper()
+	if conf.Name != name {
+		t.Errorf("wanted Name %v got %v", name, conf.Name)
+	}
+	if conf.AutoAck != autoAck {
+		t.Errorf("wanted AutoAck %v got %v", autoAck, conf.AutoAck)
+	}
+	if conf.Exclusive != exclusive {
+		t.Errorf("wanted Exclusive %v got %v", exclusive, conf.Exclusive)
+	}
+	if conf.NoLocal != noLocal {
+		t.Errorf("wanted NoLocal %v got %v", noLocal, conf.NoLocal)
+	}
+	if conf.NoWait != noWait {
+		t.Errorf("wanted NoWait %v got %v", noWait, conf.NoWait)
+	}
 }
 
 func assertPanic(t *testing.T, want error, f func()) {
