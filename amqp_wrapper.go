36dbf2152baeb7c5a897debc3653da5aeae6dd42
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2020-01-17 18:09:56 +0200

    amqp wrapper updated

--- amqp_wrapper.go
+++ amqp_wrapper.go
@@ -2,15 +2,28 @@ package istra
 
 import "github.com/streadway/amqp"
 
-func ProcessQueue(conn *amqp.Connection, conf QueueConf, f func(amqp.Delivery)) {
-	processQueue(&connectionWrapper{conn}, conf, f)
+func ConsumeQueue(conn *amqp.Connection, conf QueueConf, f func(amqp.Delivery)) {
+	consumeQueue(&connectionWrapper{conn}, conf, f)
+}
+
+func BindQueues(conn *amqp.Connection, bindings Bindings) error {
+	return bindQueues(&binderWrapper{conn}, bindings)
+}
+
+type binderWrapper struct {
+	*amqp.Connection
 }
 
 type connectionWrapper struct {
 	*amqp.Connection
 }
 
-func (cw *connectionWrapper) channel() (channel, error) {
+func (bw *binderWrapper) channel() (binder, error) {
+	ch, err := bw.Channel()
+	return &channelWrapper{ch}, err
+}
+
+func (cw *connectionWrapper) channel() (consumer, error) {
 	ch, err := cw.Channel()
 	return &channelWrapper{ch}, err
 }
@@ -23,6 +36,22 @@ type channelWrapper struct {
 	*amqp.Channel
 }
 
+func (ch *channelWrapper) close() {
+	_ = ch.Close()
+}
+
 func (ch *channelWrapper) consume(queue string, autoAck, exclusive, noLocal, noWait bool) (<-chan amqp.Delivery, error) {
 	return ch.Consume(queue, "", autoAck, exclusive, noLocal, noWait, nil)
+}
+
+func (ch *channelWrapper) declare(d DeclareConf) error {
+	return nil
+}
+
+func (ch *channelWrapper) bind(b Bind) error {
+	return nil
+}
+
+func (ch *channelWrapper) unbind(u UnBind) error {
+	return nil
 }
---
+++ amqp_wrapper_test.go
@@ -0,0 +1,26 @@
+package istra
+
+import (
+	"github.com/streadway/amqp"
+	"reflect"
+	"testing"
+)
+
+var dsn = "amqp://guest:guest@localhost:5672/"
+
+func Test_Integration(t *testing.T) {
+	conn, err := amqp.Dial(dsn)
+	if err != nil {
+		t.Errorf("Could not connect to RabbitMQ: %v", err.Error())
+		return
+	}
+	var messages []string
+	ConsumeQueue(conn, QueueConf{Name: "queue"}, func(d amqp.Delivery) {
+		messages = append(messages, string(d.Body))
+	})
+
+	want := []string{"1", "2", "3"}
+	if !reflect.DeepEqual(want, messages) {
+		t.Errorf("wanted calls %v got %v", want, messages)
+	}
+}
