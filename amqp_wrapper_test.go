be3afe74637db59d59139ae43fa6fa293f6914b3
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2020-01-21 14:50:15 +0200

    integration test

--- amqp_wrapper_test.go
+++ amqp_wrapper_test.go
@@ -10,19 +10,62 @@ import (
 
 var dsn = "amqp://guest:guest@localhost:5672/"
 
+const (
+	testQueue    = "testQueue"
+	testExchange = "testExchange"
+)
+
 func Test_Integration(t *testing.T) {
 	conn, err := amqp.Dial(dsn)
 	if err != nil {
-		t.Errorf("Could not connect to RabbitMQ: %v", err.Error())
-		return
+		t.Fatalf("didn't expected error, got '%v'", err)
 	}
+
+	err = ProcessOperations(conn, Operations{
+		ExchangeDeclare{Exchange: testExchange, Kind: "fanout"},
+		QueueDeclare{Name: testQueue},
+		Bind{Queue: testQueue, Exchange: testExchange}})
+	if err != nil {
+		t.Fatalf("didn't expected error, got '%v'", err)
+	}
+
 	var messages []string
-	ConsumeQueue(conn, QueueConf{Name: "queue"}, func(d amqp.Delivery) {
+	go ConsumeQueue(conn, QueueConf{Name: testQueue, AutoAck: true}, func(d amqp.Delivery) {
 		messages = append(messages, string(d.Body))
 	})
+
+	ch, err := conn.Channel()
+	if err != nil {
+		t.Fatalf("didn't expected error, got '%v'", err)
+	}
+
+	sendToExchange(ch, testExchange, "1")
+	sendToExchange(ch, testExchange, "2")
+	sendToExchange(ch, testExchange, "3")
+
+	err = ProcessOperations(conn, Operations{UnBind{Queue: testQueue, Exchange: testExchange}})
+	if err != nil {
+		t.Fatalf("didn't expected error, got '%v'", err)
+	}
+
+	sendToExchange(ch, testExchange, "4")
+
+	err = ch.Close()
+	if err != nil {
+		t.Fatalf("didn't expected error, got '%v'", err)
+	}
+
+	err = conn.Close()
+	if err != nil {
+		t.Fatalf("didn't expected error, got '%v'", err)
+	}
 
 	want := []string{"1", "2", "3"}
 	if !reflect.DeepEqual(want, messages) {
 		t.Errorf("wanted calls %v got %v", want, messages)
 	}
+}
+
+func sendToExchange(ch *amqp.Channel, e string, msg string) {
+	_ = ch.Publish(e, "", false, false, amqp.Publishing{ContentType: "text/plain", Body: []byte(msg)})
 }
